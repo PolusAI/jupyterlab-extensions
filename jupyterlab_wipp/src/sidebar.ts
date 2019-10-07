0ba9488fb31ba80a6780989170972506b7b91dd2
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-10-07 18:46:07 -0500

    feat: Add empty side panel

---
+++ jupyterlab_wipp/src/sidebar.ts
@@ -0,0 +1,15 @@
+import { Widget, PanelLayout } from '@phosphor/widgets';
+
+/**
+ * Sidebar widget for displaying WIPP image collections.
+ */
+export class WippSidebar extends Widget {
+    /**
+     * Create a new WIPP sidebar.
+     */
+    constructor() {
+        super();
+        //let layout = (this.layout = new PanelLayout());
+        //layout.addWidget(this._dashboard);
+    }
+}
