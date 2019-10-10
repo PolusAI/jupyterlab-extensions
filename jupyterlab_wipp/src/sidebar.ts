6065d7a8a5a2eb6099fb7d2c47889a61a9fd790b
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-10-09 21:29:52 -0400

    feat: Show real image collections in side panel

--- jupyterlab_wipp/src/sidebar.ts
+++ jupyterlab_wipp/src/sidebar.ts
@@ -1,6 +1,7 @@
 import { Widget, PanelLayout } from '@phosphor/widgets';
 import { ServerConnection } from '@jupyterlab/services';
 import { URLExt } from '@jupyterlab/coreutils';
+import { Drag } from '@phosphor/dragdrop';
 
 function ApiRequest<T>(
     url: string,
@@ -17,6 +18,21 @@ function ApiRequest<T>(
     });
   }
 
+export interface IWippImageCollection {
+    id: string, 
+    name: string,
+    creationDate: string,
+    sourceJob: string,
+    locked: boolean,
+    pattern: any,
+    numberOfImages: number,
+    imagesTotalSize: number,
+    numberImportingImages: number,
+    numberOfImportErrors: number,
+    numberOfMetadataFiles: number,
+    metadataFilesTotalSize: number,
+    _links: Object
+  }
 
 /**
  * Sidebar widget for displaying WIPP image collections.
@@ -31,127 +47,54 @@ export class WippSidebar extends Widget {
         let layout = (this.layout = new PanelLayout());
         //layout.addWidget(this._dashboard);
 
-        const wrapper = new Widget();
-        wrapper.addClass('wipp-WippSidebar-table');
-
-        //TODO: create table here
-        ///////////
-        this._table = document.createElement('table');
-
-        let fieldTitles: string[] = ['Name', 'ID', '# of images', 'Total size'];
-        // let fields: string[] = ['id', 'name'];
-        let objectArray: { 
-            id: string, 
-            name: string,
-            creationDate: string,
-            sourceJob: string,
-            locked: boolean,
-            pattern: any,
-            numberOfImages: number,
-            imagesTotalSize: number,
-            numberImportingImages: number,
-            numberOfImportErrors: number,
-            numberOfMetadataFiles: number,
-            metadataFilesTotalSize: number
-        }[] = [
-            {
-                'id': '5d92a358d62b90000918dd52',
-                'name': 'test-small',
-                'creationDate': '2019-10-01T00:52:40.870+0000',
-                'sourceJob': undefined,
-                'locked': true,
-                'pattern': undefined,
-                'numberOfImages': 30,
-                'imagesTotalSize': 81981731,
-                'numberImportingImages': 0,
-                'numberOfImportErrors': 0,
-                'numberOfMetadataFiles': 0,
-                'metadataFilesTotalSize': 0
-            },
-            {
-                'id': '5d92afe3d62b90000918e12d',
-                'name': 'sml-seg-feat-sml-egtseg-output',
-                'creationDate': '2019-10-01T01:46:11.684+0000',
-                'sourceJob': '5d92af81d62b90000918e12b',
-                'locked': true,
-                'pattern': undefined,
-                'numberOfImages': 30,
-                'imagesTotalSize': 503322900,
-                'numberImportingImages': 0,
-                'numberOfImportErrors': 0,
-                'numberOfMetadataFiles': 0,
-                'metadataFilesTotalSize': 0
-            },
-            {
-                'id': '5d92b2b2d62b90000918e14d',
-                'name': 'test-large',
-                'creationDate': '2019-10-01T01:58:10.831+0000',
-                'sourceJob': undefined,
-                'locked': true,
-                'pattern': undefined,
-                'numberOfImages': 1105,
-                'imagesTotalSize': 3420746698,
-                'numberImportingImages': 0,
-                'numberOfImportErrors': 0,
-                'numberOfMetadataFiles': 0,
-                'metadataFilesTotalSize': 0
-            }
-        ]
-
-
         // Make request to the backend API
         const settings = ServerConnection.makeSettings();
         const requestUrl = URLExt.join(settings.baseUrl, '/wipp/imageCollections');
 
-        ApiRequest<any>(requestUrl, {}, settings)
-        .then(response => {
-            console.log(response);
-          })
+        ApiRequest<IWippImageCollection[]>(requestUrl, {}, settings)
+        .then((objectArray) => {
+            let fieldTitles: string[] = ['Name', 'ID', '# of images', 'Total size'];
 
-        let thead = document.createElement('thead');
-        let thr = document.createElement('tr');
-        fieldTitles.forEach((fieldTitle) => {
-            let th = document.createElement('th');
-            th.appendChild(document.createTextNode(fieldTitle));
-            thr.appendChild(th);
-        });
-        thead.appendChild(thr);
-        this._table.appendChild(thead);
+            const wrapper = new Widget();
+            wrapper.addClass('wipp-WippSidebar-table');
 
-        let tbdy = document.createElement('tbody');
-        objectArray.forEach((object) => {
-            let tr = document.createElement('tr');
+            this._table = document.createElement('table');
 
-            var td = document.createElement('td');
-            td.appendChild(document.createTextNode(object.name));
-            tr.appendChild(td);
-            var td = document.createElement('td');
-            td.appendChild(document.createTextNode(object.id));
-            tr.appendChild(td);
-            var td = document.createElement('td');
-            td.appendChild(document.createTextNode(object.numberOfImages.toString()));
-            tr.appendChild(td);
-            var td = document.createElement('td');
-            td.appendChild(document.createTextNode(object.imagesTotalSize.toString()));
-            tr.appendChild(td);
+            let thead = document.createElement('thead');
+            let thr = document.createElement('tr');
+            fieldTitles.forEach((fieldTitle) => {
+                let th = document.createElement('th');
+                th.appendChild(document.createTextNode(fieldTitle));
+                thr.appendChild(th);
+            });
+            thead.appendChild(thr);
+            this._table.appendChild(thead);
 
-            // fields.forEach((field: string) => {
-                // var td = document.createElement('td');
-                // console.log(object.id);
-                // td.appendChild(document.createTextNode(object[field]));
-                // tr.appendChild(td);
-            // });
-            tbdy.appendChild(tr);    
-        });
+            let tbdy = document.createElement('tbody');
+            objectArray.forEach((object) => {
+                let tr = document.createElement('tr');
 
-        this._table.appendChild(tbdy);
-        ///////////
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode(object.name));
+                tr.appendChild(td);
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode(object.id));
+                tr.appendChild(td);
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode(object.numberOfImages.toString()));
+                tr.appendChild(td);
+                var td = document.createElement('td');
+                td.appendChild(document.createTextNode(object.imagesTotalSize.toString()));
+                tr.appendChild(td);
 
-        
-        //this._input = document.createElement('input');
-        //this._input.placeholder = 'DASK DASHBOARD URL';
-        wrapper.node.appendChild(this._table);
-        layout.addWidget(wrapper);
+                tbdy.appendChild(tr);    
+            });
+
+            this._table.appendChild(tbdy);
+            wrapper.node.appendChild(this._table);
+            layout.addWidget(wrapper);
+
+        })
     }
 
     private _table: HTMLTableElement;
