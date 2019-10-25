a80bef3d5645dd47a6aa1463520cbdd03679eb57
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-10-25 15:00:46 -0500

    feat: bump version

--- jupyterlab_wipp/setup.py
+++ jupyterlab_wipp/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="jupyterlab_wipp",
-    version='0.1.0',
+    version='0.2.0',
     url="https://github.com/labshare/jupyterlab-extensions/jupyterlab_wipp",
     author="Konstantin Taletskiy",
     description="WIPP integration with JupyterLab",
