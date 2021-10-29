ca7f9c66974ffe27357d4f968b6744c8175cb1cd
Konstantin Taletskiy <konstantin@taletskiy.com>
2021-10-28 20:18:49 -0700

    refactor: Small formatting changes, fixes, typos

--- jupyterlab_wipp_plugin_creator/jupyterlab_wipp_plugin_creator/handlers.py
+++ jupyterlab_wipp_plugin_creator/jupyterlab_wipp_plugin_creator/handlers.py
@@ -14,13 +14,15 @@ from .log import get_logger
 
 logger = get_logger()
 
+
 def setup_k8s_api():
     """
     Common actions to setup Kubernetes API access to Argo workflows
     """
-    config.load_incluster_config() #Only works inside of JupyterLab Pod
-    
+    config.load_incluster_config()  # Only works inside of JupyterLab Pod
+
     return client.CustomObjectsApi()
+
 
 class WippHandler(APIHandler):
     @property
@@ -28,7 +30,7 @@ class WippHandler(APIHandler):
         return self.settings["wipp-plugin-creator"]
 
 
-# Create wipp plugin based on user input
+# Create WIPP plugin based on user input
 class CreatePlugin(WippHandler):
     @tornado.web.authenticated
     def post(self):
@@ -70,7 +72,9 @@ class CreatePlugin(WippHandler):
         pluginOutputPath = os.getenv("PLUGIN_TEMP_PATH")
         # if PLUGIN_TEMP_PATH ENV exists
         if pluginOutputPath:
-            logger.info(f"PLUGIN_TEMP_PATH ENV variable exists; output path set to {pluginOutputPath}.")
+            logger.info(
+                f"PLUGIN_TEMP_PATH ENV variable exists; output path set to {pluginOutputPath}."
+            )
             pluginOutputPath = os.path.join(pluginOutputPath, f"{randomId}")
             srcOutputPath = os.path.join(pluginOutputPath, "src")
             os.makedirs(pluginOutputPath)
@@ -78,7 +82,9 @@ class CreatePlugin(WippHandler):
             logger.info(f"Random folder name created: {pluginOutputPath}.")
 
         else:
-            logger.error("PLUGIN_TEMP_PATH ENV variable doesn't exist; please use command, export PLUGIN_TEMP_PATH='...' to set.")
+            logger.error(
+                "PLUGIN_TEMP_PATH ENV variable doesn't exist; please use command, export PLUGIN_TEMP_PATH='...' to set."
+            )
             self.write_error(500)
             return
 
@@ -87,17 +93,14 @@ class CreatePlugin(WippHandler):
         # data contains 'formdata' and separately 'addedfilepaths'
         form = data["formdata"]
 
-        if "addedfilepaths" in data.keys():
-            filepaths = data["addedfilepaths"]
-        else:
-            filepaths = []
-        
+        filepaths = data.get("addedfilepaths", [])
+
         if "requirements" in form.keys():
             requirements = form["requirements"]
             # Remove requirements from form which will be written as plugin.json and write requirements.txt separately
             form.pop("requirements")
         else:
-            requirements = ""
+            requirements = []
 
         if "baseImage" in form.keys():
             baseImage = form["baseImage"]
@@ -108,44 +111,45 @@ class CreatePlugin(WippHandler):
 
         # Generate 'containerId' key to publish plugin, used as the name of the plugin to be generated
         form["containerId"] = "polusai/generated-plugins:" + randomId
-        
-        # Generate 'ui' key based on user entered input dir, used by Wipp frontend
+
+        # Generate 'ui' key based on user entered input dir, used by WIPP frontend
         uiList = []
         if form["inputs"]:
             for inp in form["inputs"]:
-                #title is set to input name as well
+                # title is set to input name as well
                 try:
                     uiKeyObj = {
-                        "key":f"inputs.{inp['name']}",
-                        "title":inp['name'],
-                        "description":inp['description']
+                        "key": f"inputs.{inp['name']}",
+                        "title": inp["name"],
+                        "description": inp["description"],
                     }
                     uiList.append(uiKeyObj)
                 except Exception as e:
-                    logger.error("Potential malformed inputs." , exc_info=e)
+                    logger.error("Potential malformed inputs.", exc_info=e)
                     self.write_error(500)
                     return
-        form['ui'] = uiList
+        form["ui"] = uiList
 
-        # register plugin manifest to wipp CI
+        # register plugin manifest to WIPP CI
         # if disable register env variable = 1 was specified by the user, don't register plugin in WIPP backend.
-        if (os.getenv("WIPP_PLUGIN_CREATOR_DISABLE_REGISTER") and (os.getenv("WIPP_PLUGIN_CREATOR_DISABLE_REGISTER")=="1")):
-            logger.info("No register mode ON. Plugin won't be registered in WIPP backend. Use 'export WIPP_PLUGIN_CREATOR_DISABLE_REGISTER=0' to enable automatic WIPP backend register.")
+        if os.getenv("WIPP_PLUGIN_CREATOR_DISABLE_REGISTER") == "1":
+            logger.info(
+                "No register mode ON. Plugin won't be registered in WIPP backend. Use 'export WIPP_PLUGIN_CREATOR_DISABLE_REGISTER=0' to enable automatic WIPP backend register."
+            )
         else:
             try:
                 self.wipp.register_plugin(form)
                 logger.info("WIPP plugin registered!")
             except Exception as e:
-                logger.error("Wipp plugin register failed," , exc_info=e)
+                logger.error("WIPP plugin register failed,", exc_info=e)
                 self.write_error(500)
-                return        
+                return
         # Get ../jupyterlab-extensions/jupyterlab_wipp_plugin_creator/jupyterlab_wipp_plugin_creator
         backendDirPath = os.path.dirname(os.path.realpath(__file__))
         templatePath = os.path.join(backendDirPath, "dockerfile.j2")
         reqsPath = os.path.join(srcOutputPath, "requirements.txt")
         manifestPath = os.path.join(pluginOutputPath, "plugin.json")
         dockerPath = os.path.join(pluginOutputPath, "Dockerfile")
-        
 
         # Generate plugin.json, aka. plugin manifest to temp folder
         try:
@@ -158,8 +162,10 @@ class CreatePlugin(WippHandler):
             template = Template(open(templatePath).read())
 
             # Generate dockerfile with user inputs, hardcoded for the time being
-            template.stream(baseImage= baseImage).dump(dockerPath)
-            logger.info(f"Dockerfile Template generated from jinja2 template, src/dockerfile.j2" )
+            template.stream(baseImage=baseImage).dump(dockerPath)
+            logger.info(
+                f"Dockerfile Template generated from jinja2 template, src/dockerfile.j2"
+            )
 
         except Exception as e:
             logger.error(f"Error writing files", exc_info=e)
@@ -173,11 +179,13 @@ class CreatePlugin(WippHandler):
                 # dedupe if the user select same code in two ways(via right click context and file manager), even though this should have been handled by frontend
                 filepaths = list(set(filepaths))
                 for filepath in filepaths:
-                    filepath =  os.path.join(os.environ['HOME'], filepath)
+                    filepath = os.path.join(os.environ["HOME"], filepath)
                     copy2(filepath, srcOutputPath)
                 logger.info("Copy command completed.")
             else:
-                logger.error(f"No file to copy. Please right click on file and select 'Add to new WIPP plugin'.")
+                logger.error(
+                    f"No file to copy. Please right click on file and select 'Add to new WIPP plugin'."
+                )
                 self.write_error(500)
                 return
 
@@ -186,13 +194,15 @@ class CreatePlugin(WippHandler):
             self.write_error(500)
             return
 
-        # if disable build env variable was specified by the user, don't execute kubernetes commands to build the actual plugin
-
-        if (os.getenv("WIPP_PLUGIN_CREATOR_DISABLE_BUILD") and (os.getenv("WIPP_PLUGIN_CREATOR_DISABLE_BUILD")=="1")):
-            logger.info("No Build mode ON. Environment is local. Plugin manifest(plugin.json) and dockerfile are generated but no images will be built. If the environment is a pod, please use 'export WIPP_PLUGIN_CREATOR_DISABLE_BUILD=0' to enable full functionality.")
+        # do not build the container if build was disabled by the user
+        if os.getenv("WIPP_PLUGIN_CREATOR_DISABLE_BUILD") == "1":
+            logger.info(
+                "The Docker image build using Argo is disabled. Plugin manifest(plugin.json) and Dockerfile will be generated but no images will be built. "
+                + "To enable the build, please use 'export WIPP_PLUGIN_CREATOR_DISABLE_BUILD=0'"
+            )
         else:
-            logger.info("No Build mode OFF. Environment is pod. Reading k8s cluster config... ")
-            try: 
+            logger.info("Reading k8s cluster config...")
+            try:
                 api_instance = setup_k8s_api()
             except Exception as e:
                 logger.error(f"Error when reading k8s config.", exc_info=e)
@@ -203,17 +213,15 @@ class CreatePlugin(WippHandler):
             logger.info(f"Beginning to run docker container via the Kubernetes Client.")
 
             # Global definition strings
-            group = 'argoproj.io' # str | The custom resource's group name
-            version = 'v1alpha1' # str | The custom resource's version
-            namespace = 'default' # str | The custom resource's namespace
-            plural = 'workflows' # str | The custom resource's plural name. For TPRs this would be lowercase plural kind.
-            
+            group = "argoproj.io"  # str | The custom resource's group name
+            version = "v1alpha1"  # str | The custom resource's version
+            namespace = "default"  # str | The custom resource's namespace
+            plural = "workflows"  # str | The custom resource's plural name. For TPRs this would be lowercase plural kind.
+
             body = {
                 "apiVersion": "argoproj.io/v1alpha1",
                 "kind": "Workflow",
-                "metadata": {
-                    "generateName": f"build-polus-plugin-{randomId}-",
-                },
+                "metadata": {"generateName": f"build-polus-plugin-{randomId}-",},
                 "spec": {
                     "entrypoint": "kaniko",
                     "volumes": [
@@ -222,19 +230,14 @@ class CreatePlugin(WippHandler):
                             "secret": {
                                 "secretName": "labshare-docker",
                                 "items": [
-                                    {
-                                        "key": ".dockerconfigjson",
-                                        "path": "config.json"
-                                    }
-                                ]
-                            }
+                                    {"key": ".dockerconfigjson", "path": "config.json"}
+                                ],
+                            },
                         },
                         {
                             "name": "workdir",
-                            "persistentVolumeClaim": {
-                                "claimName": "wipp-pv-claim"
-                            }
-                        }
+                            "persistentVolumeClaim": {"claimName": "wipp-pv-claim"},
+                        },
                     ],
                     "templates": [
                         {
@@ -242,31 +245,36 @@ class CreatePlugin(WippHandler):
                             "container": {
                                 "image": "gcr.io/kaniko-project/executor:latest",
                                 "args": [
-                                f"--dockerfile=/workspace/Dockerfile",
-                                "--context=dir:///workspace",
-                                f"--destination=polusai/generated-plugins:{randomId}"
+                                    f"--dockerfile=/workspace/Dockerfile",
+                                    "--context=dir:///workspace",
+                                    f"--destination=polusai/generated-plugins:{randomId}",
                                 ],
                                 "volumeMounts": [
                                     {
                                         "name": "kaniko-secret",
-                                        "mountPath": "/kaniko/.docker",  
+                                        "mountPath": "/kaniko/.docker",
                                     },
                                     {
                                         "name": "workdir",
                                         "mountPath": "/workspace",
-                                        "subPath": f"temp/plugins/{randomId}"
-                                    }
-                                ]
-                            }
+                                        "subPath": f"temp/plugins/{randomId}",
+                                    },
+                                ],
+                            },
                         }
-                    ]
-                }
+                    ],
+                },
             }
             try:
-                api_response = api_instance.create_namespaced_custom_object(group, version, namespace, plural, body)
+                api_response = api_instance.create_namespaced_custom_object(
+                    group, version, namespace, plural, body
+                )
                 logger.info(api_response)
             except ApiException as e:
-                logger.error("Exception when starting to build container via Kubernetes Client: %s\n" % e)
+                logger.error(
+                    "Exception when starting to build container via Kubernetes Client: %s\n"
+                    % e
+                )
                 self.write_error(500)
                 return
 
