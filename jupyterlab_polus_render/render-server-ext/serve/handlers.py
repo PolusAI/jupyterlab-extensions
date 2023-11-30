b70594b437ada5df9c3dc1f68f2530bffc50d207
jcaxle <145499292+jcaxle@users.noreply.github.com>
2023-11-30 00:56:13 -0500

    JupyterLab Polus Render 0.1.0 (#39)

---
+++ jupyterlab_polus_render/render-server-ext/serve/handlers.py
@@ -0,0 +1,31 @@
+"""Jupyter server example handlers."""
+from jupyter_server.base.handlers import JupyterHandler
+from jupyter_server.extension.handler import ExtensionHandlerJinjaMixin, ExtensionHandlerMixin
+import tornado
+from tornado.web import StaticFileHandler
+
+class DefaultHandler(ExtensionHandlerMixin, JupyterHandler):
+    """Default API handler."""
+
+    @tornado.web.authenticated
+    def get(self, path):
+        self.write("<h1>render-server-ext</h1>")
+        self.write("For usage and more, please click <a href=\"https://github.com/PolusAI/jupyterlab-extensions\">here</a>")
+
+class AuthFileHandler(JupyterHandler, StaticFileHandler):
+
+    def _initialize(self,path) -> None:
+        StaticFileHandler._initialize(path)
+
+
+class BaseTemplateHandler(ExtensionHandlerJinjaMixin, ExtensionHandlerMixin, JupyterHandler):
+    """The base template handler."""
+
+    pass
+
+class ErrorHandler(BaseTemplateHandler):
+    """An error handler."""
+
+    def get(self, path):
+        """Write_error renders template from error.html file."""
+        self.write_error(400)
