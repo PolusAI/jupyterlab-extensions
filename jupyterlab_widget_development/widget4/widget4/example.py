c3b536043db0481a62199898d5dd2733b27b35b3
Akshat Saini <akshatsaini1@gmail.com>
2024-02-07 17:09:58 -0500

    Updated example.py and widget.ts

--- jupyterlab_widget_development/widget4/widget4/example.py
+++ jupyterlab_widget_development/widget4/widget4/example.py
@@ -9,7 +9,7 @@ TODO: Add module docstring
 """
 
 from ipywidgets import DOMWidget
-from traitlets import Unicode
+from traitlets import Unicode, Integer
 from ._frontend import module_name, module_version
 
 
@@ -21,7 +21,7 @@ class ExampleWidget(DOMWidget):
     _view_module = Unicode(module_name).tag(sync=True)
     _view_module_version = Unicode(module_version).tag(sync=True)
     imageUrl = Unicode('').tag(sync=True)
-    height = Unicode('').tag(sync=True)
+    height = Integer(0).tag(sync=True)
 
     def __init__(self, imageUrl='', height='', **kwargs):
         super().__init__(**kwargs)
