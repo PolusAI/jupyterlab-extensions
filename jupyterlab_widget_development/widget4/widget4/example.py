0450a26496153ef6b4a9cee4a235ec49831fa200
Akshat Saini <akshatsaini1@gmail.com>
2024-02-13 14:57:47 -0500

    Added pathlib module functionality

--- jupyterlab_widget_development/widget4/widget4/example.py
+++ jupyterlab_widget_development/widget4/widget4/example.py
@@ -8,7 +8,8 @@
 TODO: Add module docstring
 """
 
-import os
+# import os
+from pathlib import Path
 from ipywidgets import DOMWidget
 from traitlets import Unicode, Integer
 from ._frontend import module_name, module_version
@@ -28,15 +29,25 @@ class ExampleWidget(DOMWidget):
     def __init__(self, imagePath='', height=0, **kwargs):
         super().__init__(**kwargs)
 
-        notebook_dir = os.path.dirname(os.path.realpath(__name__))
+# Commented code below uses OS module to grab system path and perform checks
+        
+        # notebook_dir = os.path.dirname(os.path.realpath(__name__))
+        # self.imagePath = imagePath
+        # self.height = height
+        # full_image_path = imagePath
+        # # Check for sys path or relative path
+        # if os.path.isabs(imagePath):
+        #     full_image_path = imagePath
+        # else:   
+        #     full_image_path = os.path.join(notebook_dir, imagePath)
+        # self.full_image_path = full_image_path
+
+# Below uses pathlib module
+        
+        notebook_dir = Path.cwd() # Get the current working directory
         self.imagePath = imagePath
         self.height = height
-        full_image_path = imagePath
-        # Check for sys path or relative path
-        if os.path.isabs(imagePath):
-            full_image_path = imagePath
-        else:   
-            full_image_path = os.path.join(notebook_dir, imagePath)
-        self.full_image_path = full_image_path
-
-        
+        full_image_path = Path(imagePath) # Convert imagePath to a Path object
+        if not full_image_path.is_absolute():
+            full_image_path = notebook_dir / imagePath
+        self.full_image_path = str(full_image_path) # Convert to string object
