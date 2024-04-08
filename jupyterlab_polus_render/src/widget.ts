9e2d0510ffbfbcd8dc104c70bff9bd35771fbfe6
Konstantin Taletskiy <konstantin.taletskiy@axleinfo.com>
2024-04-08 10:24:27 -0700

    Refactor renderPlugin as JupyterFrontEndPlugin (#47)

--- jupyterlab_polus_render/src/widget.ts
+++ jupyterlab_polus_render/src/widget.ts
@@ -2,21 +2,15 @@
 
 import {
   DOMWidgetModel,
-  DOMWidgetView,
   ISerializers,
 } from '@jupyter-widgets/base';
 
 import { MODULE_NAME, MODULE_VERSION } from './version';
-import { PageConfig } from '@jupyterlab/coreutils';
-import { store } from '@labshare/polus-render';
 
 // Import the CSS
 import '../css/widget.css';
 
-// Get the base URL of the JupyterLab session
-const baseUrl = PageConfig.getBaseUrl();
-// URL for serving images
-const renderFilePrefix = 'jupyterlab-polus-render/image'
+
 
 export class RenderModel extends DOMWidgetModel {
   defaults() {
@@ -42,43 +36,4 @@ export class RenderModel extends DOMWidgetModel {
   static view_name = 'RenderView'; // Set to null if no view
   static view_module = MODULE_NAME; // Set to null if no view
   static view_module_version = MODULE_VERSION;
-}
-
-export class RenderView extends DOMWidgetView {
-  render() {
-    let imagePath = this.model.get('imagePath');
-    let overlayPath = this.model.get('overlayPath');
-    let isImagePathUrl = this.model.get('is_imagePath_url');
-    let isOverlayPathUrl = this.model.get('is_overlayPath_url');
-    let imageUrl = isImagePathUrl ? imagePath : `${baseUrl}${renderFilePrefix}${imagePath}`; // T/F condition ? valueIfTrue : valueIfFalse
-    let overlayUrl = isOverlayPathUrl ? overlayPath : `${baseUrl}${renderFilePrefix}${overlayPath}`;
-
-    // Set the image url
-    store.setState({
-      urls: [
-        imageUrl,
-      ],
-    });
-
-    // Set the overlay url
-    fetch(overlayUrl).then((response) => {
-      response.json().then((overlayData) => {
-        store.setState({
-          overlayData,
-        });
-        const heatmapIds = Object.keys(overlayData.value_range)
-          .map((d: any) => ({ label: d, value: d }))
-          .concat({ label: 'None', value: null });
-    
-        store.setState({
-          heatmapIds,
-        });
-      });
-    });    
-
-    this.el.innerHTML = `
-    <div style="width:100%;height:900px">
-    <polus-render></polus-render>
-    `;
-  }
 }
